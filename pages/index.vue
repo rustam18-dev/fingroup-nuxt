70b3c5e61b90119175b279e62b3ee687f2de95a6
john_front_dev <jamshid200404@gmail.com>
2024-01-09 13:33:54 +0500

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@ useHead({
 </script>
 
 <template>
-  <div class="container mx-auto px-[100px] relative">
+  <div class="container mx-auto 2xl:px-[100px] relative">
     <Navbar />
 
     <!-- Back_objects -->
